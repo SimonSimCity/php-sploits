ab2559ee9576e566c6af161a5b4c9385bf390757
Simon Schick <simonsimcity@gmail.com>
2020-12-30 11:57:57 +0100

    Update editprofile.php
    Uploading of malicious files - images which contain PHP code in their meta-data

--- editprofile.php
+++ editprofile.php
@@ -6,6 +6,57 @@ if(!empty($_SESSION['authed']) && $_SESSION['authed'] === true) {
     if(!empty($_SESSION['userid'])) {
 
         if($_SERVER['REQUEST_METHOD'] == "POST") {
+            if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Upload') {
+              if (isset($_FILES['uploadedFile']) && $_FILES['uploadedFile']['error'] === UPLOAD_ERR_OK)
+              {
+                // get details of the uploaded file
+                $fileTmpPath = $_FILES['uploadedFile']['tmp_name'];
+                $fileName = $_FILES['uploadedFile']['name'];
+                $fileSize = $_FILES['uploadedFile']['size'];
+                $fileType = $_FILES['uploadedFile']['type'];
+                $fileNameCmps = explode(".", $fileName);
+                $fileExtension = strtolower(end($fileNameCmps));
+
+                // sanitize file-name
+                $newFileName = md5(time() . $fileName) . '.' . $fileExtension;
+
+                // check if file has one of the following extensions
+                $allowedfileExtensions = array('gif', 'png');
+                $allowedMimeTypes = array('image/gif', 'image/png');
+
+                if (in_array($fileExtension, $allowedfileExtensions) && in_array(mime_content_type($fileTmpPath), $allowedMimeTypes))
+                {
+                  // directory in which the uploaded file will be moved
+                  $uploadFileDir = './uploaded_files/';
+                  $dest_path = $uploadFileDir . $newFileName;
+
+                  if(move_uploaded_file($fileTmpPath, $dest_path))
+                  {
+                    echo 'File is successfully uploaded.';
+                    $updateSQL = "update users set profile_url = '" . $dest_path
+                                 . "' where id = " .  $_SESSION['userid'];
+
+                    $updated = insertQuery($updateSQL, true);
+                  }
+                  else
+                  {
+                    echo 'There was some error moving the file to upload directory. Please make sure the upload directory is writable by web server.';
+                  }
+                }
+                else
+                {
+                  echo 'Upload failed. Allowed file types: ' . implode(',', $allowedfileExtensions);
+                  echo ' Allowed mime types: ' . implode(',', $allowedMimeTypes);
+                  echo ' Your file is: ' . $fileExtension . ' - ' . mime_content_type($fileTmpPath);
+                }
+              }
+              else
+              {
+                echo 'There is some error in the file upload. Please check the following error.<br>';
+                echo 'Error:' . $_FILES['uploadedFile']['error'];
+              }
+            }
+
             if(!empty($_REQUEST['firstname']) && !empty($_REQUEST['surname'])
                 && !empty($_REQUEST['email'])) {
 
@@ -23,7 +74,7 @@ if(!empty($_SESSION['authed']) && $_SESSION['authed'] === true) {
             }
         }
         else {
-            $userSQL  = "select email, firstname, surname from users where id = " .  $_SESSION['userid'];
+            $userSQL  = "select email, firstname, surname, profile_url from users where id = " .  $_SESSION['userid'];
             $userList = getSelect($userSQL);
 
             if(empty($userList) && is_array($userList)) {
@@ -31,7 +82,7 @@ if(!empty($_SESSION['authed']) && $_SESSION['authed'] === true) {
             }
             $user = $userList[0];
         ?>
-        <form method="POST">
+        <form method="POST" enctype="multipart/form-data">
             <p>Edit your settings</p>
             <label for="firstname">Firstname:</label>
             <input name="firstname" id="firstname" value="<?=$user[1]?>" /> <br />
@@ -40,6 +91,12 @@ if(!empty($_SESSION['authed']) && $_SESSION['authed'] === true) {
             <label for="email">Email:</label>
             <input name="email" id="email" value="<?=$user[0]?>" /> <br />
             <input type="submit" value="Update profile">
+            <br />
+            <br />
+            <label for="profile_url">Profile:</label>
+            <img src="<?=$user[3]?>" /> <br />
+            <input type="file" id="file-upload" name="uploadedFile"> <br />
+            <input type="submit" name="uploadBtn" value="Upload" />
         </form>
         <?
         }
